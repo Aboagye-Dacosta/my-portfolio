c3e255829ee315d66f97e3caddf33f8533234b57
Dacosta <dacsolo10@gmail.com>
2024-04-11 22:32:13 +0000

    reduced spinner with on mobile devices

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components";
+import { breakpoint } from "../styles/GlobalStyles";
 
 const rotate = keyframes`
   to {
@@ -8,8 +9,8 @@ const rotate = keyframes`
 
 const Spinner = styled.div`
   margin: 4.8rem auto;
+  width: 7.4rem;
 
-  width: 10.4rem;
   aspect-ratio: 1;
   border-radius: 50%;
   background: radial-gradient(farthest-side, var(--color-brand-700) 94%, #0000)
@@ -17,6 +18,10 @@ const Spinner = styled.div`
     conic-gradient(#0000 30%, var(--color-brand-700));
   -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 10px), #000 0);
   animation: ${rotate} 1.5s infinite linear;
+
+  @media screen and (min-width: ${breakpoint.tablet}) {
+    width: 10.4rem;
+  }
 `;
 
 export default Spinner;
